e5350cd637606fda34226a0991634eda16be8121
Rachagani-Vishnu-2603 <Rachagani-Vishnu-2603@users.noreply.github.com>
2020-05-22 13:23:35 +0530

    Code file created.

---
+++ GreyAtom/code.py
@@ -0,0 +1,71 @@
+# --------------
+# Code starts
+# Create the lists 
+class_1=['Geoffrey Hinton','Andrew Ng','Sebastian Raschka','Yoshua Bengio']
+class_2=['Hilary Mason','Carla Gentry','Corinna Cortes']
+new_class=class_1+class_2
+new_class.append('Peter Warden')
+print(new_class)
+new_class.remove('Carla Gentry')
+print(new_class)
+courses={'Math':65,'English':70,'History':80,'French':70,'Science':60}
+total=sum(list(courses.values()))
+percentage=(total*0.2)
+print(percentage)
+mathematics={'Geoffrey Hinton':78,'Andrew Ng':95,'Sebastian Raschka':65,'Yoshua Benjio':50,'Hilary Mason':70,'Corinna Cortes':66,'Peter Warden':75}
+# Concatenate both the strings
+topper=max(mathematics,key=mathematics.get)
+first_name,last_name=topper.split(" ")
+full_name=last_name+" "+first_name
+certificate_name=full_name.upper()
+print(certificate_name)
+# Append the list
+
+# Print updated list
+
+
+# Remove the element from the list
+
+# Print the list
+
+
+
+# Create the Dictionary
+
+
+
+# Slice the dict and stores  the all subjects marks in variable
+
+
+# Store the all the subject in one variable `Total`
+
+# Print the total
+
+# Insert percentage formula
+
+# Print the percentage
+
+
+
+
+# Create the Dictionary
+ 
+
+
+
+# Given string
+
+
+# Create variable first_name 
+
+# Create variable Last_name and store last two element in the list
+
+# Concatenate the string
+
+# print the full_name
+
+# print the name in upper case
+
+# Code ends here
+
+
